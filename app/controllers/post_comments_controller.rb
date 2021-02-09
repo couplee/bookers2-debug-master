025738fc88e80154af4e3d1567a1b6aef70607b3
couplee <couplee888@gmail.com>
2021-02-09 09:42:30 +0000

    [Finish] 6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -4,17 +4,20 @@ class PostCommentsController < ApplicationController
     @post_comment = current_user.post_comments.new(post_comment_params)
     @post_comment.book_id = @book.id
     if @post_comment.save
-      redirect_to book_path(@book.id)
+      # redirect_to book_path(@book.id)
     else
       @user = @book.user
       @new_book = Book.new
-      render "books/show"
+      # render "books/show"
     end
   end
   
   def destroy
+    @book = Book.find(params[:book_id])
+    # @post_comment_id = @book.id
+    # @post_comment = @book.post_comments.find(params[:id])
     PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    # redirect_to book_path(params[:book_id])
   end
   
   private
